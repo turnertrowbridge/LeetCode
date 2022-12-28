624537e9c0e22eaa2b0a9c6a96b0b963454e4178
Turner Trowbridge <ttrowbridge7322@sdsu.edu>
2022-12-27 19:46:55 -0800

    Time: 248 ms (77.39%), Space: 25.4 MB (30.20%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.py
+++ 0189-rotate-array/0189-rotate-array.py
@@ -4,15 +4,45 @@ class Solution:
         Do not return anything, modify nums in-place instead.
         """
 
-        # brute force 0(n) * k
+        # ** brute force **
+        # time complexitiy: O(n) * k 
+        # space complexity: O(1)
+
         # for i in range(0, k):
         #     nums.insert(0, nums[-1])
         #     nums.pop()
-            
-        rotated_array = [0] * len(nums)
 
-        for i in range(len(nums)):
-            rotated_array[(i + k) % len(nums)] = nums[i]    # adds items to array, and overflow gets 
-                                                            # added to front
 
-        nums[:] = rotated_array
+        # ** using another array**
+        # time complexitiy: O(n) 
+        # space complexity: O(n)
+
+
+        # rotated_array = [0] * len(nums)
+
+        # for i in range(len(nums)):
+        #     rotated_array[(i + k) % len(nums)] = nums[i]    # adds items to array, and overflow gets 
+        #                                                     # added to front
+
+        # nums[:] = rotated_array
+
+
+        # leet code solution: cyclic replacements
+        # time complexitiy: O(n)
+        # space complexity: O(1)
+
+        n = len(nums)
+        k %= n
+
+        start = count = 0
+        while count < n:
+            current, prev = start, nums[start]
+            while True:
+                next_idx = (current + k) % n
+                nums[next_idx], prev = prev, nums[next_idx] # swap elements
+                current = next_idx 
+                count += 1
+
+                if start == current:
+                    break
+            start += 1 

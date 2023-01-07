634289e2b72990aecb6afa8e4f9a8b8b7243e3b6
Turner Trowbridge <ttrowbridge7322@sdsu.edu>
2023-01-06 16:30:27 -0800

    Time: 46 ms (74.96%), Space: 14.1 MB (74.71%) - LeetHub

--- 0682-baseball-game/0682-baseball-game.py
+++ 0682-baseball-game/0682-baseball-game.py
@@ -4,16 +4,10 @@ class Solution:
 
         for o in operations:        
                 if o == "+" and len(stack) > 1:
-                    s1 = stack.pop()
-                    s2 = stack.pop()
-                    stack.append(s2)
-                    stack.append(s1)
-                    stack.append(s1 + s2)
+                    stack.append(stack[-1] + stack[-2])
 
                 elif o == "D" and stack:
-                    s1 = stack.pop()
-                    stack.append(s1)
-                    stack.append(s1 * 2)
+                    stack.append(stack[-1] * 2)
 
                 elif o == "C" and stack:
                     stack.pop()
